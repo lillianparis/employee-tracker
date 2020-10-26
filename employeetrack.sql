ad33edf119fb1a60b1d84781d14c912be53f87f7
Lillian Paris <lillian.paris529@gmail.com>
2020-10-25 19:44:50 -0700

    Created tables for department, role and employee

--- employeetrack.sql
+++ employeetrack.sql
@@ -2,3 +2,26 @@ DROP DATABASE IF EXISTS employeeDB
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
+
+CREATE TABLE department(
+  id INT PRIMARY KEY,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NULL,
+  salary VARCHAR(30) NULL,
+  department_id VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT ,
+  manager_id INT ,
+  PRIMARY KEY (id)
+);
