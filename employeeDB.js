8a87586391c51cfe9f3d3579cd344985304ea25f
Lillian Paris <lillian.paris529@gmail.com>
2020-10-25 20:00:37 -0700

    Created starter questions for command line

--- employeeDB.js
+++ employeeDB.js
@@ -14,10 +14,33 @@ var connection = mysql.createConnection({
   database: "employeeDB"
 });
 
+// connects to mysql server and sql database
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
-
 // installed npm install, npm init -y and npm install mysql (10/25/2020)
+// Build a command-line application that at a minimum allows the user to:
+//   * Add departments, roles, employees
+//   * View departments, roles, employees
+//   * Update employee roles 
+
+// Creating a function prompting the user what action they should take...
+function start () {
+    inquirer 
+    .prompt({
+        name: "actionrequired",
+        type: "Question",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"],
+        default: "Use arrow keys"
+    })
+    // Based on the users answer, they will be prompted with their response.
+    .then(function(answer) {
+        if (answer.viewAllEmployees === "View All Employees"){
+
+        }
+    })
+}
